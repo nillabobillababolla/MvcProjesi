293bb4fc7e27f2f21462efca25f86d91f2f68c0a
Samet Alabey <alabeysamet@gmail.com>
2019-02-13 14:49:19 +0300

    Migrations enabled.

---
+++ TeknikServis.DAL/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+namespace TeknikServis.DAL.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TeknikServis.DAL.MyContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(TeknikServis.DAL.MyContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
