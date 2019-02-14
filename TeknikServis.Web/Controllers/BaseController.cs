790f72d3c0939584f61dd19d29743623cc695f5b
Samet Alabey <alabeysamet@gmail.com>
2019-02-14 13:44:18 +0300

    AdminController eklendi.

---
+++ TeknikServis.Web/Controllers/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using TeknikServis.BLL.Identity;
+
+namespace TeknikServis.Web.Controllers
+{
+    [Authorize]
+    [RequireHttps]
+    public class BaseController : Controller
+    {
+        protected List<SelectListItem> GetRoleList()
+        {
+            var data = new List<SelectListItem>();
+            MembershipTools.NewRoleStore().Roles
+                .ToList()
+                .ForEach(x =>
+                {
+                    data.Add(new SelectListItem()
+                    {
+                        Text = $"{x.Name}",
+                        Value = x.Id
+                    });
+                });
+            return data;
+        }
+    }
+}
