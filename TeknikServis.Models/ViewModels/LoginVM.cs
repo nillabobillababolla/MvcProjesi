20251b3b50fce05004203ff7133ada095bbdb5c8
Samet Alabey <alabeysamet@gmail.com>
2019-02-17 18:33:29 +0300

    Hata mesajı düzenleme ve checbox. #1

--- TeknikServis.Models/ViewModels/LoginVM.cs
+++ TeknikServis.Models/ViewModels/LoginVM.cs
@@ -5,8 +5,10 @@ namespace TeknikServis.Models.ViewModels
     public class LoginVM
     {
         [Required]
+        [MinLength(1,ErrorMessage ="Kullanıcı Adı alanı gereklidir!")]
         [Display(Name = "Kullanıcı Adı")]
         public string UserName { get; set; }
+
         [StringLength(10, MinimumLength = 5, ErrorMessage = "Şifreniz 5-10 karakter arası olmalıdır!")]
         [Display(Name = "Şifre")]
         [DataType(DataType.Password)]
